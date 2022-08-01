fa49ed42348e1a189ac90feca14f930f1506b0ea
alina <sapron4ik93@gmail.com>
2022-08-01 14:29:48 -0500

    Home page started

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Link, Routes } from "react-router-dom";
-import {Register, Login} from "./";
+import {Register, Login, Home} from "./";
+
 
 
 const App = () =>{
     return(
+        
     <div><h1>Hello World</h1>
     <Routes>
+        <Home/>
         <Route exact path = "/Register" element={<Register />}></Route>
         <Route exact path="/Login" element={<Login />}></Route>
     </Routes>
---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <div>
+            <div><h1>Welcome to Fitness Trackr</h1></div>
+            <div>Navbar</div>
+            <div>
+                <p>Login part</p>
+               <img>picture</img> 
+            </div>
+            <div>Footer</div>
+        </div>
+    )
+
+}
+
+export default Home;
