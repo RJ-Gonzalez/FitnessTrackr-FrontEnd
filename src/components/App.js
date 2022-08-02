24336bb30af63ffa92dc4f4094d766d6138da410
Ruby Gonzalez <RubyJ.Gonzalez@outlook.com>
2022-08-01 19:44:01 -0500

    Working on Homepage

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,12 @@ import {Register, Login, Home} from "./";
 const App = () =>{
     return(
         
-    <div><h1>Hello World</h1>
+    <div>
+        <h6>Just here for App reference</h6>
     <Routes>
-        <Home/>
         <Route exact path = "/Register" element={<Register />}></Route>
         <Route exact path="/Login" element={<Login />}></Route>
+        <Route exact path="/Home" element={<Home />}></Route>
     </Routes>
     </div>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const Home = () => {
+import "./style.css";
+
+export default function Home () {
 
     return (
+//         // <div>
+//         //     <div><h1>Welcome to Fitness Trackr</h1></div>
+//         //     <div>Navbar</div>
+//         //     <div>
+//         //         <p>Login part</p>
+//         //        <img>picture</img> 
+//         //     </div>
+//         //     <div>Footer</div>
+//         // </div>
+        // <div className="p-5 bg-dark mb-4">
         <div>
-            <div><h1>Welcome to Fitness Trackr</h1></div>
-            <div>Navbar</div>
-            <div>
-                <p>Login part</p>
-               <img>picture</img> 
-            </div>
-            <div>Footer</div>
+        <div className="navbar navbar-expand-lg navbar-light bg-dark">
+        {/* <nav className="d-flex"> */}
+        <div className="container-fluid">
+   {/* <div  className="text-white"><h1>Welcome to Fitness Trackr</h1></div> */}
+          <h4 className="mb-0">
+            <Link to="/Login" className="text-white">Login</Link>
+            <span className="text-white"> | </span>
+            <Link to="/Register" className="text-white">Register</Link>
+            <span className="text-white"> | </span>
+            <Link to="/Register" className="text-white">Routines</Link>
+            <span className="text-white"> | </span>
+            <Link to="/Register" className="text-white">Activities</Link>
+          </h4>
+          </div>
+      </div>
+
+
+        <img src = "https://www.31fss.com/templates/yootheme/cache/Fitness-Centers-HERO-ed9cae28.jpeg"/>
+
+
+
+
+
+      <footer className="bg-dark text-center text-white">
+        <div className="container p-4 pb-0">
+          <section className="mb-4">
+          </section>
         </div>
+        <div className="text-center p-3" style={{ backgroundColor: "black" }}>
+          Alina Sapronova + Ruby Gonzalez © 2022
+        </div>
+      </footer>
+      </div>
+        
     )
 
 }
 
-export default Home;
+// // export default Home;
